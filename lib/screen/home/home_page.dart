f6ce31efe78fe35ed451c7b1ac96836487f4789d
IgorBrat <akbars5678@gmail.com>
2022-05-26 21:56:33 +0300

    Gets levels from database, and they get tests from db

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lang_app/screen/test/level.dart';
 import 'package:lang_app/screen/test/test_page.dart';
+
+import '../../core/database.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,23 +18,35 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MaterialButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Level();
-                  }));
-                },
-                child: const Text("Level1"),
-                color: Colors.orangeAccent,
-              ),
-            ],
-          )
-      ),
+    return StreamBuilder<QuerySnapshot>(
+      stream: DatabaseService().levelsCollection.snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData)
+          return const Center(
+            child: Text("LOADING."),
+          );
+        return Center(
+          child: _buildLevelsList(snapshot.data!),
+        );
+      },
+    );
+  }
+
+  Widget _buildLevelsList(QuerySnapshot snapshot) {
+    return ListView.builder(
+      itemCount: snapshot.docs.length,
+      itemBuilder: (context, index) {
+        final doc = snapshot.docs[index];
+        return MaterialButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return Level(levelName: doc["name"]);
+            }));
+          },
+          child: Text(doc["name"]),
+          color: Colors.orangeAccent,
+        );
+      },
     );
   }
 }
--- lib/screen/test/level.dart
+++ lib/screen/test/level.dart
@@ -7,7 +7,9 @@ import 'package:lang_app/screen/test/test_page.dart';
 import '../../core/database.dart';
 
 class Level extends StatefulWidget {
-  const Level({Key? key}) : super(key: key);
+  const Level({Key? key, required String this.levelName}) : super(key: key);
+
+  final String levelName;
 
   @override
   State<Level> createState() => _LevelState();
@@ -19,7 +21,7 @@ class _LevelState extends State<Level> {
     return StreamBuilder<QuerySnapshot>(
       stream: DatabaseService().testsCollection.snapshots(),
       builder: (context, snapshot) {
-        if(!snapshot.hasData) return Text("No news.");
+        if(!snapshot.hasData) return const Text("LOADING.");
         return Center(
           child: _buildLevel(snapshot.data!),
         );
@@ -29,7 +31,7 @@ class _LevelState extends State<Level> {
 
   Widget _buildLevel(QuerySnapshot snapshot){
     return ListView.builder(
-      itemCount: snapshot.docs.length,
+      itemCount: 5,
       itemBuilder: (context, index){
         final doc = snapshot.docs[index];
         return TestPage(options: doc["options"], right: doc["correct"], callback: (right)=> log(right.toString()));
