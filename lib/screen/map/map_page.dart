1bdce96b74641fcb20102eb5c0df049dd7fb9622
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2022-06-12 21:24:11 +0300

    Middle progress on map

--- lib/screen/map/map_page.dart
+++ lib/screen/map/map_page.dart
@@ -1,3 +1,4 @@
+import 'package:expansion_widget/expansion_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
@@ -9,36 +10,88 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPage extends State<MapPage> {
+  Widget buildTile(context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        width: 60,
+        height: 60,
+        alignment: Alignment.centerRight,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/attractions.png'),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(fit: StackFit.expand, children: <Widget>[
-      Scaffold(
-          floatingActionButton: SpeedDial(
-            icon: Icons.menu,
-            activeIcon: Icons.close,
-            backgroundColor: const Color(0xff0068C9),
-            foregroundColor: Colors.white,
-            activeBackgroundColor: const Color(0xFFF2D84F),
-            activeForegroundColor: Colors.white,
-            buttonSize: 56.0,
-            visible: true,
-            closeManually: false,
-            curve: Curves.bounceIn,
-            overlayColor: Colors.black,
-            overlayOpacity: 0.5,
-
-            children: [
-              SpeedDialChild(
-                //speed dial child
-                  child: const Icon(Icons.accessibility),
-                  label: 'Language'
+    return Container(
+      constraints: BoxConstraints.expand(),
+      child: Stack(
+        children: [
+          Scaffold(
+              floatingActionButton: SpeedDial(
+                icon: Icons.menu,
+                activeIcon: Icons.close,
+                backgroundColor: const Color(0xff0068C9),
+                foregroundColor: Colors.white,
+                activeBackgroundColor: const Color(0xFFF2D84F),
+                activeForegroundColor: Colors.white,
+                buttonSize: 56.0,
+                visible: true,
+                closeManually: false,
+                curve: Curves.bounceIn,
+                overlayColor: Colors.black,
+                overlayOpacity: 0.5,
+                children: [
+                  SpeedDialChild(
+                      //speed dial child
+                      child: const Icon(Icons.accessibility),
+                      label: 'Language'),
+                ],
               ),
-            ],
+              body: Container()),
+          Image.asset('assets/images/ornament.png', fit: BoxFit.cover),
+          Image.asset('assets/images/ukraine.png'),
+          ConstrainedBox(
+            constraints:
+                BoxConstraints(maxWidth: 120, minWidth: 90, minHeight: 80),
+            child: ExpansionWidget(
+              titleBuilder:
+                  (double animationValue, _, bool isExpaned, toogleFunction) {
+                return InkWell(
+                    onTap: () => toogleFunction(animated: true),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Transform.rotate(
+                            angle: 3.14 * animationValue / 2,
+                            child: Icon(Icons.arrow_right, size: 40),
+                            alignment: Alignment.center,
+                          ),
+                          buildTile(context),
+                        ],
+                      ),
+                    );
+              },
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  buildTile(context),
+                  buildTile(context),
+                  buildTile(context),
+                  buildTile(context),
+                  buildTile(context),
+                ],
+              ),
+            ),
           ),
-          body: Container()),
-      Image.asset('assets/images/ornament.png', fit: BoxFit.cover),
-      Image.asset('assets/images/ukraine.png'),
-    ],
+        ],
+      ),
     );
   }
 }
